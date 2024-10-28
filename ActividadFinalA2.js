5728952b1bbf0680350ae6662d70d459680c817b
antoniolavinarenas <antoniolavinarenas@gmail.com>
2024-10-28 17:41:10 +0100

    He añadido un comentario al código

--- ActividadFinalA2.js
+++ ActividadFinalA2.js
@@ -18,5 +18,6 @@ function comercial(tIngr){
     }
     return name;
 }
+// nom hace referencia al nombre y fact a lo facturado cada mes del año (hay 12 valores)
 let comer = [ {nom:"Daniel",fact:[6899,2713,6827,5805,3058,3679,4094,7581,5867,6901,5116,2392]}, {nom:"Raquel",fact:[6747,5485,7431,2667,2431,3900,7222,7601,5109,6556,6211,7087]}, {nom:"Eva",fact:[2831,2645,2242,5586,3124,7396,5556,3268,3186,6632,5319,2263]}, {nom:"Víctor",fact:[4758,6212,7835,6240,6513,6639,4246,3244,5914,3338,2415,5240]}, ];
 console.log(comercial(comer));
